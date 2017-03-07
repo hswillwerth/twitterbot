690e5d9173fd0a27ac129f45d3a9e3810edab60c
Hanna Willwerth <hannawillwerth@Hannas-MacBook-Air.local>
2017-03-07 18:03:52 -0500

    PUT YOUR COMMIT MESSAGE HERE

--- tweetbot2.py
+++ tweetbot2.py
@@ -1,14 +1,14 @@
 from twython import Twython
 import csv
 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_TOKEN_SECRET = ''
+CONSUMER_KEY = 'pb9hqugm8DgPpX4e0hDRvndB9'
+CONSUMER_SECRET = 'wbXFYN94IhDX9vjuQdwyF9ALBOxIsEjbioo4bDYUDDhZGynHMs'
+ACCESS_TOKEN = '316821018-3veCZGMl2i7XT5OZmAlGT0TwCnkpLbxALmcwTjED'
+ACCESS_TOKEN_SECRET = '9WkJuhcsLuJbgBbEu0eiX1nRUjaheLfcUdPZ5yvsmAJ0M'
 
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-search = twitter.search(q='YOUR SEARCH TERM HERE', count="100")
+search = twitter.search(q='Guatemala', count="100")
 tweets = search['statuses']
 
 with open ('data.csv', 'w') as fp:
@@ -21,5 +21,5 @@ with open ('data.csv', 'w') as fp:
             url = result['entities']['urls'][0]['expanded_url']
         except:
             url = None
-        text=[['propublica', result['text'].encode('utf-8'), url]]
+        text=[['Guatemala', result['text'].encode('utf-8'), url]]
         a.writerows((text))
